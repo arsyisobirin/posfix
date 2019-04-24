0bf7ef5a1db25a08985a2221fb5d5d74da62dbba
arsyisobirin <48671439+arsyisobirin@users.noreply.github.com>
2019-04-25 05:26:44 +0700

    Update infixposfix.cpp

--- infixposfix.cpp
+++ infixposfix.cpp
@@ -2,3 +2,19 @@
 //Fadhel Riyantyo	    (1817051065)
 //M Arsyi Sobirin	    (1817051066)
 //Achmad Kurnia Chandra(1817051070)*/
+
+#include <iostream>
+using namespace std;
+
+struct Element{
+   string Q[1000];
+   int top;
+  
+};
+
+class myStack {
+  private:
+    Element tumpuk;
+    Element *head = NULL;
+  
+  public:
